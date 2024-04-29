31d11c7dc1fa0727c023eafe8dc4051c134d7d41
Anarchick <34984003+Anarchick@users.noreply.github.com>
2024-04-29 22:58:02 +0200

    save

--- src/main/java/fr/anarchick/cani/internal/CanI.java
+++ src/main/java/fr/anarchick/cani/internal/CanI.java
@@ -1,4 +1,17 @@
 package fr.anarchick.cani.internal;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Used when a method contain canI events.
+ * You have to specify the events class that are used in the method.
+ */
+@Documented
+@Target(ElementType.METHOD)
 public @interface CanI {
+
+    Class<? extends CanIEvent>[] value();
+
 }
